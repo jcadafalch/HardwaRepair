22fc01a04a61d57fc2c508edd380e6fb7307e41a
Lucas Tolón Pachecho <tolon.pacheco.lucas@alumnat.copernic.cat>
2022-03-16 08:11:30 +0100

    Crear Categoria / model categoria

---
+++ src/main/java/cat/copernic/HardwaRepair/Model/categoria.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.copernic.HardwaRepair.Model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+/**
+ *
+ * @author lktol
+ */
+
+@Data
+@Entity
+@Table(name="categoria")
+public class categoria implements Serializable{
+    
+    private static final long serialVersionUID=2L;
+    
+    @Id
+    @GeneratedValue (strategy=GenerationType.IDENTITY)
+    private String id;
+    
+    @NotEmpty(message="No has introduït el nom")
+    private String nom;
+    
+    
+    
+}
