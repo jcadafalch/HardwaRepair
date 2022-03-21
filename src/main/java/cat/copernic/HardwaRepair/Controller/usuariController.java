f79ab4b23e363ce6f87cb9570c1b166ecc2bb734
Lucas Tolón Pachecho <tolon.pacheco.lucas@alumnat.copernic.cat>
2022-03-21 09:38:59 +0100

    Controlador Usuario + Controlador Categoria

---
+++ src/main/java/cat/copernic/HardwaRepair/Controller/usuariController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.copernic.HardwaRepair.Controller;
+
+import cat.copernic.HardwaRepair.DAO.usuariDAO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author Lucas Tolón Pacheco
+ */
+@org.springframework.stereotype.Controller
+@Slf4j
+public class usuariController {
+    @Autowired
+    private usuariDAO usuariDAO;
+    
+    @GetMapping("/controller")
+    public String inici(Model model) {
+        log.info("Executant el controlador de Usuari");
+        
+        var usuaris = usuariDAO.findAll();
+        
+        model.addAttribute("usuaris", usuaris);
+        
+        return "inici";
+    }
+}
--- src/main/resources/serveis/categoriaServiceInterface.java
+++ src/main/resources/serveis/categoriaServiceInterface.java
@@ -11,11 +11,11 @@ import java.util.List;
 //Interface on definirem els mètodes CRUD personalitzats per la nostra aplicació
 public interface categoriaServiceInterface {
     
-    public List<categoria> llistarCategoria(); //Mètode que implementarem per llistar gossos
+    public List<categoria> llistarCategoria(); //Mètode que implementarem per llistar Categories
     
-    public void afegirCategoria(categoria categoria); //Mètode que implementarem per afegir un gos
+    public void afegirCategoria(categoria categoria); //Mètode que implementarem per afegir un categoria
     
-    public void eliminarCategoria(categoria categoria); //Mètode que implementarem per eliminar un gos
+    public void eliminarCategoria(categoria categoria); //Mètode que implementarem per eliminar un categoria
     
-    public categoria cercarCategoria(categoria categoria); //Mètode que implementarem per cercar un gos
+    public categoria cercarCategoria(categoria categoria); //Mètode que implementarem per cercar un categoria
 }
