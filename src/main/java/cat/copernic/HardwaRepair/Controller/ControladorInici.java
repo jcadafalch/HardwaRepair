dac0f62c676bafb62a096c713724739882248a82
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2022-03-28 09:07:05 +0200

    jcm - removed unsefull comments

--- src/main/java/cat/copernic/HardwaRepair/Controller/ControladorInici.java
+++ src/main/java/cat/copernic/HardwaRepair/Controller/ControladorInici.java
@@ -1,10 +1,5 @@
 package cat.copernic.HardwaRepair.Controller;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import cat.copernic.HardwaRepair.Model.Usuari;
 import cat.copernic.HardwaRepair.serveis.UsuariServiceInterface;
 import java.util.List;
