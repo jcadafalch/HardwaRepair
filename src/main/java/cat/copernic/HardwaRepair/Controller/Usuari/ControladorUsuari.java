dc4274319829653e45f4f0e53645d4bcb7dccb8f
Lucas Tolón Pachecho <tolon.pacheco.lucas@alumnat.copernic.cat>
2022-04-06 00:52:23 +0200

    Import errors controlador Usuaris

--- src/main/java/cat/copernic/HardwaRepair/Controller/Usuari/ControladorUsuari.java
+++ src/main/java/cat/copernic/HardwaRepair/Controller/Usuari/ControladorUsuari.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
