a937896be1177df4e9386eb6dac3f4e2b2811e72
Mxrc14 <mhs1277@gmail.com>
2022-04-17 00:43:21 +0200

    Errors Bootstrap

--- src/main/java/cat/copernic/HardwaRepair/Model/Incidencia.java
+++ src/main/java/cat/copernic/HardwaRepair/Model/Incidencia.java
@@ -36,7 +36,7 @@ public class Incidencia implements Serializable{
     private static final long serialVersionUID=1L;
 
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)   
+//    @GeneratedValue(strategy=GenerationType.IDENTITY)   
     @NotEmpty
     private String id_incidencia;
    
