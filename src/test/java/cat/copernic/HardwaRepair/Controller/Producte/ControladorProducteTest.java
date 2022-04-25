8c5dfa90a6eaf923a02b18a6f4c3578b6938de31
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2022-04-25 19:42:58 +0200

    producte Testing

---
+++ src/test/java/cat/copernic/HardwaRepair/Controller/Producte/ControladorProducteTest.java
@@ -0,0 +1,25 @@
+package cat.copernic.HardwaRepair.Controller.Producte;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControladorProducteTest {
+
+    @Test
+    void llistarProductes() {
+
+    }
+
+    @Test
+    void crearFormulariProducte() {
+    }
+
+    @Test
+    void guardarProducteCrear() {
+    }
+
+    @Test
+    void eliminarProducte() {
+    }
+}
